4204a90a21319f43a29f039858187a080f814b04
Aras Can Akin <aacanakin@gmail.com>
2018-10-09 13:20:21 +0300

    Improve saga template

--- src/templates/store/saga.t.ts
+++ src/templates/store/saga.t.ts
@@ -1,26 +1,28 @@
+import { capitalize, uppercase } from "../string.utils";
 
 export function generateSaga(name: string) {
     return (
     `
+// TODO: Add saga middleware definition to store/index.ts
 // import { put, takeEvery, call } from "redux-saga";
 
 // worker Saga: will be fired on USER_FETCH_REQUESTED actions
-// function* fetchUser(action) {
+// function* fetch${capitalize(name)}(action) {
 //     try {
 //         // TODO: make your api call here
 //         //const user = yield call(Api.fetchUser, action.payload.userId);
-//         yield put({ type: "USER_FETCH_SUCCEEDED", user: user });
+//         yield put({ type: "${uppercase(name)}_FETCH_SUCCEEDED", ${name}: ${name} });
 //     } catch (e) {
-//         yield put({ type: "USER_FETCH_FAILED", message: e.message });
+//         yield put({ type: "${uppercase(name)}_FETCH_FAILED", message: e.message });
 //     }
 // }
 
 /*
-  Starts fetchUser on each dispatched \`USER_FETCH_REQUESTED\` action.
+  Starts fetchUser on each dispatched \`${uppercase(name)}_FETCH_REQUESTED\` action.
   Allows concurrent fetches of user.
 */
-// function* mySaga() {
-//     yield takeEvery("USER_FETCH_REQUESTED", fetchUser);
+// function* ${name}Saga() {
+//     yield takeEvery("${uppercase(name)}_FETCH_REQUESTED", fetch${capitalize(name)});
 // }
 
 `
